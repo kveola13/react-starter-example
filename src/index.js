fde4620b1b41a07f2c4074237a9668f9e692c464
ola <kveola13@student.westerdals.no>
2020-09-01 08:27:16 +0200

    new implementations

--- src/index.js
+++ src/index.js
@@ -2,23 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
+function Square(props){
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
-      );
-    }
-  }
+    )
+}
   
   class Board extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        }
+    }
+
+
+    handleClick(i){
+        const squares = this.state.squares.slice()
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            squares : squares,
+            xIsNext: !this.state.xIsNext
+        })
+    }
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return <Square value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}/>;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
@@ -56,6 +74,22 @@ class Square extends React.Component {
           </div>
         </div>
       );
+    }
+  }
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      
     }
   }
   
