78077f8c2ef8c0ced6a0eb2f47f3160c00202287
ola <kveola13@student.westerdals.no>
2020-09-01 10:37:41 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -12,15 +12,6 @@ function Square(props){
   
   class Board extends React.Component {
 
-    constructor(props){
-        super(props)
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        }
-    }
-
-
     handleClick(i){
         const squares = this.state.squares.slice()
         if(calculateWinner(squares) || squares[i]){
@@ -34,21 +25,13 @@ function Square(props){
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}/>;
+      return <Square value={this.props.squares[i]}
+      onClick={() => this.props.handleClick(i)}/>;
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares)
-      let status
-      if(winner) {
-        status = 'Winner ' + winner
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-      }
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -85,10 +68,12 @@ function Square(props){
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}/>
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
